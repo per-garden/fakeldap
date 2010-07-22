d925ff0fc311c62a2535cf585cca39a3d4a0637a
Aanand Prasad <aanand.prasad@gmail.com>
2010-07-22 15:45:53 -0400

    Didn't need those.

--- lib/fakeldap.rb
+++ lib/fakeldap.rb
@@ -34,13 +34,6 @@ module FakeLDAP
       raise LDAP::ResultError::InappropriateAuthentication, "This server does not support anonymous bind" unless dn
       raise LDAP::ResultError::InvalidCredentials, "Invalid credentials" unless @server.valid_credentials?(dn, password)
     end
-
-    def search(basedn, scope, deref, filter);               raise "not implemented"; end
-    def modify(dn, modification);                           raise "not implemented"; end
-    def add(dn, av);                                        raise "not implemented"; end
-    def del(dn);                                            raise "not implemented"; end
-    def modifydn(entry, newrdn, deleteoldrdn, newSuperior); raise "not implemented"; end
-    def compare(entry, attr, val);                          raise "not implemented"; end
   end
 end
 
